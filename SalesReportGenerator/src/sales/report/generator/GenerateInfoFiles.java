ac69e53216b060680035ebf751d2e23e8a2d6d91
Daniela Alexandra Chamorro Guerrero <dalexach@gmail.com>
2024-09-10 01:27:03 -0500

    New files and adding comments for funcions

--- SalesReportGenerator/src/sales/report/generator/GenerateInfoFiles.java
+++ SalesReportGenerator/src/sales/report/generator/GenerateInfoFiles.java
@@ -3,8 +3,18 @@ package sales.report.generator;
 import java.io.File;
 import java.io.FileWriter;
 
+/**
+ * The GenerateInfoFiles class is responsible for generating random data files 
+ * used in the sales report system. It creates folders, files, and writes 
+ * random information for products and sellers into flat files.
+ */
 public class GenerateInfoFiles {
 	
+	/**
+     * Creates a folder in the specified path.
+     *
+     * @param folderName the name of the folder to be created
+     */	
 	public static void createFolder(String folderName) {
 		
 		File folderM = new File(folderName);
@@ -12,6 +22,11 @@ public class GenerateInfoFiles {
 
 	}
 	
+	 /**
+     * Creates a new empty file.
+     *
+     * @param file the path and name of the file to be created
+     */
 	public static void createFile(String file) {
 		File fileM = new File(file);
 		
@@ -23,6 +38,13 @@ public class GenerateInfoFiles {
 		
 	}
 
+	/**
+     * Writes a specified number of random products into a file. Each product 
+     * includes an ID, name, and price.
+     *
+     * @param file the path and name of the file to write the products to
+     * @param numberOfProducts the number of products to generate
+     */
 	public static void writeProductsFile(String file, int numberOfProducts) {
 		File fileM = new File(file);
 		
@@ -33,8 +55,8 @@ public class GenerateInfoFiles {
                 String productId = String.valueOf(i + 1000);
                 String productName = DataSource.createNewProduct();
                 double productPrice = DataSource.createRandomPrice();
-
-                // Escribir la línea en el formato solicitado
+                
+                // Writing the line in the requested format
                 writer.write(productId + ";" + productName + ";" + String.format("%.2f", productPrice) + "\n");
             }
             System.out.println("Documento generado correctamente en " + file);
@@ -46,6 +68,13 @@ public class GenerateInfoFiles {
 		}
 	}
 	
+	/**
+     * Writes a specified number of random sellers into a file. Each seller 
+     * includes a document type, document number, first name, and last name.
+     *
+     * @param file the path and name of the file to write the sellers to
+     * @param numberOfSellers the number of sellers to generate
+     */
 	public static void writeSellersFile(String file, int numberOfSellers) {
 		File fileM = new File(file);
 		
@@ -71,11 +100,12 @@ public class GenerateInfoFiles {
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		/*Clase Main que al ejecutarse debe generar los archivos 
-		planos pseudoaleatorios que servirán como entrada para la ejecución de la segunda clase con 
-		método main*/
+		/**
+	     * Main method that creates the necessary folders and files, and generates
+	     * random product and seller data for testing purposes.
+	     *
+	     * @param args command-line arguments (not used)
+	     */
 
 		String folder = "mainRequiredFiles";
 		String fileProducts = "mainRequiredFiles/Products";
